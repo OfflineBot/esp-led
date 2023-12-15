1baec42ce2a1a5c343c554c3c736a9c7e8dd4b9c
OfflineBot <flintstonelg@gmail.com>
2023-12-15 10:31:52 +0100

    cookies

--- data/static/js/main.js
+++ data/static/js/main.js
@@ -12,14 +12,17 @@ render_preview();
 red.addEventListener('input', (event) => {
     red_val = red.value;
     render_preview();
+    setCookie("red", red_val, 1, "None");
 });
 green.addEventListener('input', (event) => {
     green_val = green.value;
     render_preview();
+    setCookie("green", green_val, 1, "None");
 });
 blue.addEventListener('input', (event) => {
     blue_val = blue.value;
     render_preview();
+    setCookie("blue", blue_val, 1, "None");
 });
 
 function rgbToHex(red, green, blue) {
@@ -42,3 +45,49 @@ function render_preview() {
     preview.style.borderColor = color;
 }
 
+
+function setCookie(name, value, days, sameSite) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+
+    var sameSiteAttribute = sameSite ? "; SameSite=" + sameSite : "";
+
+    // Include "Secure" attribute when SameSite is set to "None"
+    var secureAttribute = sameSite === "None" ? "; Secure" : "";
+
+    document.cookie = name + "=" + value + expires + "; path=/" + sameSiteAttribute + secureAttribute;
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return 0;
+}
+
+
+window.onload = function() {
+    let red_value = getCookie("red");
+    let green_value = getCookie("green");
+    let blue_value = getCookie("blue"); 
+
+    red.value = red_value;
+    green.value = green_value;
+    blue.value = blue_value; 
+    red_val = red_value;
+    green_val = green_value;
+    blue_val = blue_value;
+    render_preview();
+};
